b0a424ecbb67aab5b5d6299b0bb3411b16f3f3a7
yutaueda <yuta.u1969@gmail.com>
2019-12-27 11:31:33 +0900

    test commit for send-grid setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.default_url_options   = { host: 'mysterious-crag-78895.herokuapp.com' }
+  ActionMailer::Base.smtp_settings           = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      authentication:       :plain,
+      user_name:            ENV['SENDGRID_USERNAME'],
+      password:             ENV['SENDGRID_PASSWORD'],
+      domain:               'heroku.com',
+      enable_starttls_auto: true
+  }
 end
